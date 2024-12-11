0f00b74a32979bd34b851bf736a9c0bdb92064b3
Mnavaa <navamargareth@gmail.com>
2024-12-11 17:35:19 +0100

    Classe aposta

---
+++ casino/src/main/java/Model/Aposta.java
@@ -0,0 +1,21 @@
+package Model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author isard
+ */
+public class Aposta {
+    private int id;
+    private String nomUsuari;
+    private String enfrontament;
+    private String resultat;
+    private Date datapartit;
+    private double apostaEconomica;
+    
+    
+}

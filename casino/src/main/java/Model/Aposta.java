25d6dc678a66587633a39dc77f24523d3038100e
Mnavaa <navamargareth@gmail.com>
2024-12-13 15:45:08 +0100

    Getters y setters

--- casino/src/main/java/Model/Aposta.java
+++ casino/src/main/java/Model/Aposta.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Date;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -19,7 +21,7 @@ public class Aposta {
     
     public Aposta(){} //Constructor buit
     
-    public Aposta(int id, String nomUsuari, String enfrontament, String resultat, Date datapartit, double apostaEconomica){
+    public Aposta(int id, String nomUsuari, String enfrontament, String resultat, Date dataPartit, double apostaEconomica){
         this.id = id;
         this.nomUsuari = nomUsuari;
         this.enfrontament = enfrontament;
@@ -27,7 +29,43 @@ public class Aposta {
         this.dataPartit = dataPartit;
         this.apostaEconomica = apostaEconomica;
     }
-    
-    
-    
-}
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getNomUsuari(){
+        return nomUsuari;
+        
+    }
+    public void setNomUsuari(String nomUsuari){
+        this.nomUsuari = nomUsuari;
+    }
+    public String getEnfrotament(){
+        return enfrontament;
+    }
+    public void setEnfrontament(String enfrontament){
+        this.enfrontament = enfrontament;
+    }
+    public String setResultat(){
+        return resultat;
+    }
+    public void setResultat(String resultat){
+       this.resultat = resultat; 
+    }
+    public Date getDataPartit(){
+        return dataPartit;
+    }
+    public void setDataPartit(Date dataPartit){
+        this.dataPartit = dataPartit;
+    }
+    public double getApostaEconomica(){
+        return apostaEconomica;
+    }
+    public void setApostaEconomica(double apostaEconomica){
+        this.apostaEconomica = apostaEconomica;
+    }
+    @Override
+    public S
+}   

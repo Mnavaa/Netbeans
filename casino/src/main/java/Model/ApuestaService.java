cd7d233dd2e8676dfc097f3150a41f510951c222
Mnavaa <navamargareth@gmail.com>
2024-12-17 02:31:19 +0100

    borrar y mostrar

--- casino/src/main/java/Model/ApuestaService.java
+++ casino/src/main/java/Model/ApuestaService.java
@@ -3,10 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletRequest;
 
       
 
@@ -15,13 +14,34 @@ import javax.servlet.http.HttpServletRequest;
  * @author isard
  */
 public class ApuestaService {
-public class ApuestaService {List<Apuesta>listaApuestas, int contadorID, HttpServletRequest request) {
+        public void addApuesta {List<Apuesta>listaApuestas, int contadorID, HttpServletRequest request) {
     try {
             String nombre = request.getParameter("nombre");
             String equipo = request.getParameter("equipo");
             String resultat = request.getParameter("Resultat");
             double apuesta = Double.parseDouble(request.getParameter("apuesta"));
 
-       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         
+             LocalDate fecha_partido = LocalDate.parse(request.getParameter("fecha_partido"), formatter);
     
+            Apuesta nuevaApuesta = new Apuesta(nombre, contadorID, resultat, fecha_partido, apuesta, equipo);
+            listaApuestas.add(nuevaApuesta);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+        public void Borrar(List<Apuesta> listaApuestas, HttpServletRequest request) {
+        int ID = Integer.parseInt(request.getParameter("ID"));
+        listaApuestas.removeIf(apuesta -> apuesta.getID() == ID);
+    }
+
+    public void Mostrar(List<Apuesta> listaApuestas, HttpServletRequest request) {
+        int ID = Integer.parseInt(request.getParameter("ID"));
+        for (Apuesta aposta : listaApuestas) {
+            if (aposta.getID() == ID) {
+                request.setAttribute("apuesta", aposta);
+                break;
+            }
+        }
+    }
